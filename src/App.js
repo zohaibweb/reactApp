6d7884f0b55c904be8f03bec6273a4c5068e2825
Muhammad Zohaib <zohaibfas@gmail.com>
2021-02-04 15:21:00 +0500

    some initial things are done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState } from 'react'
+import AddTask from './components/AddTask'
+
 
 
 
@@ -27,10 +28,33 @@ function App() {
         reminder: false
     }
   ])
+  // Add Tasks
+
+  const addTask = task => {
+    const id = Math.floor(Math.random()* 100000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Deleting Task
+
+const deleteTask = id => {
+  setTasks(tasks.filter(task => task.id !== id))
+}
+
+// Toggle Reminder
+
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map( (task) => task.id === id ? {...task, reminder: !task.reminder } : task)
+  )
+}
+
   return (
     <div className="container">
      <Header />
-     <Tasks tasks={tasks}/>
+     <AddTask onAdd={addTask} />
+     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle = {toggleReminder}/> : 'yup !!! you have completed your all tasks'}
     </div>
   );
 }

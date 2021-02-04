f02aa62185892b3039283caca94b04278bf705c6
Muhammad Zohaib <zohaibfas@gmail.com>
2021-02-04 20:31:04 +0500

    developing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import AddTask from './components/AddTask'
 
 
 function App() {
-
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id:1,
@@ -52,8 +52,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-     <Header />
-     <AddTask onAdd={addTask} />
+     <Header onAdd={()=> setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+     {showAddTask && <AddTask onAdd={addTask} />}
      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle = {toggleReminder}/> : 'yup !!! you have completed your all tasks'}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,13 @@
 
 import Buttons from './Buttons'
 
-const Header = () => {
+const Header = ({onAdd, showAdd}) => {
 
-    const onClick = () => {
-        console.log('clicked');
-    }
+   
     return (
         <header className='header'>
             <h1>Task Tracker</h1>
-            <Buttons text='Add' color= 'green' onClick={onClick} />
+            <Buttons text={showAdd ? 'close' : 'Add'} color= {showAdd ? 'red' : 'green'} onClick={onAdd} />
         </header>
     )
 }
